2fea095ad753da712b7bfad29856e9b358584673
Matheus Carrasco <159260622+matheuscarrasco@users.noreply.github.com>
2024-07-31 11:28:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-let legendas = document.getElementById ("legenda")
-let imagens = document.getElementById ("imagem")
-function Imagem1()
-{
-    document.getElementById ("legenda").innerHTML= <P>"Times Square"</P>;
-    document.getElementById ("imagem").innerHTML = "<img src='caption.jpg'>"
+function conteudo1() {
+    document.getElementById("conteudo").innerHTML = "<P>A Estátua da Liberdade não ficou em 1° lugar, porque mais do que representar Nova York, a Estátua da Liberdade talvez seja o grande ponto turístico que representa os Estados Unidos. </P>";
+    document.getElementById("imagem").innerHTML = "<img src='Estatua.jpg'>"
 }
 
-function Imagem2 ()
-{
-    legendas.innerHTML="Predio"
-    imagens.innerHTML = "view.jpg"
+function conteudo2() {
+    document.getElementById("conteudo").innerHTML = "<P>De dia e de noite. Visite a Times Square. Parece besteira citar a Times Square como ponto turístico, mas muitos leitores esquecem de incluir o cruzamento mais famoso do mundo no roteiro de viagem em Nova York. A Times Square é um cruzamento entre a Broadway e a 42nd Street.  </P>"
+    document.getElementById("imagem").innerHTML = "<img src='Times.jpg'>"
 }
 
-function Imagem3 ()
-{
-    legendas.innerHTML="Neve"
-    imagens.innerHTML = "neve.jpg"
+function conteudo3() {
+    document.getElementById("conteudo").innerHTML = "<P>A ponte do Brooklyn é sem dúvidas um dos pontos turísticos mais marcantes de Nova York. Seja a pé, de bicicleta ou então de carro, cruze a ponte do Brooklyn é uma grande emoção.</P>";
+    document.getElementById("imagem").innerHTML = "<img src='Ponte.jpg'>"
 }
 
-function Imagem4 ()
-{
-    legendas.innerHTML="estatua"
-    imagens.innerHT = "estatua.avif"
+function conteudo4() {
+    document.getElementById("conteudo").innerHTML = "<P>Aqui já passaram King Kong, presidentes, atrizes e atores de Hollywood e milhares de turistas, o Empire State Building é realmente um grande ícone de Nova York.  O Empire State Building foi inaugurado em 1931 e por 40 anos foi o prédio mais alto do mundo.</P>";
+    document.getElementById("imagem").innerHTML = "<img src='Empire.jpg'>"
 }

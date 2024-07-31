194e57a248519689fc4578a83bd091b8c1be394f
Matheus Carrasco <159260622+matheuscarrasco@users.noreply.github.com>
2024-07-31 09:23:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let legendas = document.getElementById ("legenda")
+let imagens = document.getElementById ("imagem")
+function Imagem1()
+{
+    document.getElementById ("legenda").innerHTML= <P>"Times Square"</P>;
+    document.getElementById ("imagem").innerHTML = "<img src='caption.jpg'>"
+}
+
+function Imagem2 ()
+{
+    legendas.innerHTML="Predio"
+    imagens.innerHTML = "view.jpg"
+}
+
+function Imagem3 ()
+{
+    legendas.innerHTML="Neve"
+    imagens.innerHTML = "neve.jpg"
+}
+
+function Imagem4 ()
+{
+    legendas.innerHTML="estatua"
+    imagens.innerHT = "estatua.avif"
+}
